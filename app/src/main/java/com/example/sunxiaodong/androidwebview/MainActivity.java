be2d33af3a6fd5b9ac6e53046f0855c429d29edc
sunxiaodongme <hys_521@126.com>
2016-03-29 11:44:14 +0800

    Android WebView使用示例
    Android WebView使用示例

---
+++ app/src/main/java/com/example/sunxiaodong/androidwebview/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.sunxiaodong.androidwebview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mWebView = (WebView) findViewById(R.id.webview);
+        final String contentUrl = "http://59.42.210.75:9880/iyourcar_news/news?aid=78";
+        mWebView.loadUrl(contentUrl);
+        final WebSettings webSettings = mWebView.getSettings();
+        mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webSettings.setUseWideViewPort(true);// 设置此属性，可任意比例缩放
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setJavaScriptEnabled(true);  //支持js
+        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);  //设置 缓存模式
+        // 开启 DOM storage API 功能
+        webSettings.setDomStorageEnabled(true);
+        //开启 database storage API 功能
+        webSettings.setDatabaseEnabled(true);
+        String cacheDirPath = getFilesDir().getAbsolutePath() + "/webcache";
+        //设置数据库缓存路径
+        webSettings.setDatabasePath(cacheDirPath);
+        //设置  Application Caches 缓存目录
+        webSettings.setAppCachePath(cacheDirPath);
+        //开启 Application Caches 功能
+        webSettings.setAppCacheEnabled(true);
+
+        webSettings.setBlockNetworkImage(true);
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+
+            }
+        });
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.equals(contentUrl)) {
+                    view.loadUrl(url);
+                }
+                return true;
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode,
+                                        String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                webSettings.setBlockNetworkImage(false);
+            }
+        });
+    }
+
+}
